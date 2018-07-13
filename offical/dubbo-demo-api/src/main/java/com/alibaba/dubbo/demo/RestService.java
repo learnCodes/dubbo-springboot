6aa8f0e29341324aa10f7aaed9ddbbd3393194df
iZDT <dev@dthappy.com>
2018-07-13 17:30:03 +0800

    change http annotation to  interface for use rest from conusmer

--- offical/dubbo-demo-api/src/main/java/com/alibaba/dubbo/demo/RestService.java
+++ offical/dubbo-demo-api/src/main/java/com/alibaba/dubbo/demo/RestService.java
@@ -1,6 +1,22 @@
 package com.alibaba.dubbo.demo;
 import com.alibaba.dubbo.demo.entity.HelloEntity;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
+
+@Path("demo")
 public interface RestService {
-    HelloEntity sayHello(String name);
+    @GET
+    @Path("hello/{name}")
+    //@Consumes({MediaType.APPLICATION_JSON})    
+    @Produces({MediaType.APPLICATION_JSON})   
+    HelloEntity testHello(@PathParam("name") String name);
+    /*
+     * Doc:
+     * http://dubbo.apache.org/#/docs/user/references/protocol/rest.md?lang=zh-cn
+     */
 }
--- offical/dubbo-springboot-consumer/src/main/java/com/alibaba/demo/consumerboot/controller/HelloWorldController.java
+++ offical/dubbo-springboot-consumer/src/main/java/com/alibaba/demo/consumerboot/controller/HelloWorldController.java
@@ -5,13 +5,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.alibaba.dubbo.demo.DemoService;
+import com.alibaba.dubbo.demo.RestService;
+import com.alibaba.dubbo.demo.entity.HelloEntity;
 
 @RestController
 public class HelloWorldController{
     @Autowired
-    private DemoService service;
+    private DemoService demoService;
+    @Autowired
+    private RestService restService;
     @RequestMapping("/hello/{name}")
     public String helloWorld(@PathVariable("name") String name) {
-        return service.sayHello(name);
+        return demoService.sayHello(name);
     }
+    @RequestMapping("/rest/{name}")
+    public String helloRest(@PathVariable("name") String name) {
+        HelloEntity entity = restService.testHello(name);
+        return entity.getMessage()+" , from "+entity.getIp()+":"+entity.getPort();
+    } 
 }
